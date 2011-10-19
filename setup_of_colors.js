01c6a9eb105e0b57353f488bb700d09860bcf538
Ondřej Bartas <o.bartas@gmail.com>
2011-10-19 16:35:00 +0200

    [Change design

---
+++ setup_of_colors.js
@@ -0,0 +1,21 @@
+#1
+	Pixastic.process(img1, "hsl", {hue:-180,saturation:100,lightness:0});
+#2
+	Pixastic.process(img1, "hsl", {hue:-60,saturation:100,lightness:0});
+#3
+	Pixastic.process(img1, "hsl", {hue:0,saturation:100,lightness:0});
+#4
+	Pixastic.process(img1, "hsl", {hue:60,saturation:100,lightness:0});
+#5
+	Pixastic.process(img1, "hsl", {hue:120,saturation:100,lightness:0});
+#6
+	Pixastic.process(img1, "hsl", {hue:-150,saturation:100,lightness:0});
+
+
+colors = new Array();
+colors[1] = "-180";
+colors[2] = "-60";
+colors[3] = "0";
+colors[4] = "60";
+colors[5] = "120";
+colors[6] = "-150";
